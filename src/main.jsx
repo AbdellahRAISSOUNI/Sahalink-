9b9582bbce9747cfabb7732087376da6d7e62900
AbdellahRAISSOUNI <abdellahraissouni@gmail.com>
2025-01-11 12:43:50 +0100

    initialize project structure with essential files and configurations

---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import ReactDOM from "react-dom/client";
+
+import { BrowserRouter as Router } from "react-router-dom";
+
+import App from "./App";
+import "./index.css";
+
+import { PrivyProvider } from "@privy-io/react-auth";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <PrivyProvider
+    appId="cm5pjyh6n0e2pd6x4uzuv8ced"
+    config={{
+      // Customize Privy's appearance in your app
+      appearance: {
+        theme: "dark",
+      },
+    }}
+  >
+    <Router>
+      <App />
+    </Router>
+  </PrivyProvider>,
+);
